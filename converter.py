c4ab3565da4887c51b19ec67783a863cd351905a
moussa <abdulrahman.ibrahim@aucegypt.edu>
2019-04-21 11:17:49 +0200

    bugs fixes + parsing - phase2

--- converter.py
+++ converter.py
@@ -6,22 +6,9 @@ Created on Sat Apr 20 15:43:52 2019
 """
 
 
-import numpy as np
 import json as json
 
 
-class macro:
-    size = 0  ;
-    
-class layer:
-    direction = "" ;
-    pitch = "" ;
-    width = "" ;
-    spacing = "" ;
-    offset = "" ;
-    
-macros = [];
-layers = [] ;
 
 with open('mux4x1_json') as f:
     data = json.load(f)
@@ -34,16 +21,71 @@ lef = open( 'osu035.lef' , 'r' );
     
 # print(data['modules']['mux4x1']['ports']['a']) ;  
 
-layers.append(layer);
-layer.pitch = "34";
+case = 0 ;
+type = "null" ;
+via_layer = "null" ;
+layers_metal = {} ;
+vias = {} ;
 
 
 with open('osu035.lef') as mylef:  
-   for line in mylef:
-       try:
-           first = line.split()[0] ;
-           if( first == 'LAYER'):
-               case = 1 ;
-       except:
-           pass
-       
+    for line in mylef:
+
+        #keep looking for cases
+        if (case == 0 ):
+            try:
+                first = line.split()[0];
+                second = line.split()[1];
+                if( first == 'LAYER'):
+                   case = 1 ; # inside metal layer
+                   #print(second);
+                   if( second=='metal1' or type=='metal2' or type=='metal3' or type=='metal4' or  type=='metal5' or  type=='metal6' ):
+                        type = second;
+                        layers_metal[second] = {} ;
+                if( first == 'VIA' ):
+                    case = 2 ; #inside VIA layer
+                    if ( second=="M2_M1" or second=="M3_M2" or second=="M4_M3" or second=="M5_M4" or second=="M5_M6" ) :
+                        type = second ;
+                        vias[second] = {} ;
+
+            except:
+                pass
+
+        #inside a case
+        else:
+            try:
+                first = line.split()[0];
+                second = line.split()[1];
+                # inside first case (layer case)
+                if (first == "END"):
+                    case = 0 ;  # not inside any of the cases
+                    type = "null";
+                    via_layer = "null" ;
+                else:
+                    #print(first, second, case, type)
+                    if (case == 1): #metal Layer
+                        if( type=='metal1' or type=='metal2' or type=='metal3' or type=='metal4' or  type=='metal5' or  type=='metal6' ):
+                            if( first == "PITCH" ):
+                                layers_metal[type]["pitch"] = second;
+                            if (first == "WIDTH"):
+                                layers_metal[type]["width"] = second;
+                            if (first == "SPACING"):
+                                layers_metal[type]["spacing"] = second;
+                            if (first == "OFFSET"):
+                                layers_metal[type]["offset"] = second;
+                    if ( case == 2 ) : #Vias
+                        if( type=="M2_M1" or type=="M3_M2" or type=="M4_M3" or type=="M5_M4" or type=="M5_M6" ) :
+                            if (first=="LAYER"):
+                                via_layer = second
+                            else:
+                                vias[type][via_layer] =  line.rstrip(";\n\r") ;
+
+
+                    # if (case == 2):
+                    #     print(first , second) ;
+            except:
+                pass
+
+
+print(layers_metal);
+print(vias);
